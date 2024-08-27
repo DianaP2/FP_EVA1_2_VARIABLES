169c41c4c8830b65a3c718d30761cf9a32538b91
DianaP2 <L24550326@outlook.com>
2024-08-26 22:13:20 -0600

    Add files via upload

---
+++ EVA1_2_VARIABLE/src/eva1_2_variable/EVA1_2_VARIABLE.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_variable;
+
+/**
+ *
+ * diana sanchez
+ */
+public class EVA1_2_VARIABLE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo"); //cadena de texto
+        System.out.println(100);  //entero
+        System.out.println(3.1416);  //numero con punto flotante
+        System.out.println('c');  //caracter
+        System.out.println(true);  //valores logicos
+        System.out.println(false);
+        
+        
+        //Enteros: int, long, short
+        //Numeros con parte decimal: float, double
+        //Cadena de texto: String
+        //Caracters: char
+        //Valores logicos: boolean
+        
+        //Declaracion de variables, sintaxi:
+       String nombre;
+       String apellido;
+       String curp;
+       double promedio;
+       int edad;
+       
+       edad = 17;
+       
+       nombre = "Diana Paola";
+       apellido = "Sanchez Flores";
+       promedio = 8.6;
+       
+       System.out.println(edad);
+       System.out.println(nombre);
+       System.out.println(apellido);
+       System.out.println(promedio);
+        
+        
+    }
+    
+}
